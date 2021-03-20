ef00b9a5938d4f8248ea75aafcc5d0b5cf7b50c5
pearlcube <sfa.shiga@gmail.com>
2021-03-20 14:34:28 +0900

    初回

---
+++ WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Text
+Imports System.IO
+Imports Newtonsoft.Json
+
+Class MainWindow
+
+    Private Sub Button1_Click(sender As Object, e As RoutedEventArgs)
+        'If text1.Text = "CHANGE" Then
+        '    text1.Text = "HELLO"
+        'Else
+        '    text1.Text = "CHANGE"
+        'End If
+
+        Dim data1 = New SampleData With
+        {
+          .Description = "サンプル",
+          .UpdateDate = DateTimeOffset.Now,
+          .Data = New Dictionary(Of Integer, String) From
+          {
+            {1, "データ1"}, {2, "データ2"}
+          }
+        }
+
+        Dim output As String = JsonConvert.SerializeObject(data1)
+        Dim deserialized = JsonConvert.DeserializeObject(Of SampleData)(output)
+
+        Dim sjisEnc As Encoding = Encoding.GetEncoding("Unicode")
+        Dim writer As StreamWriter = New StreamWriter("Test.txt", True, sjisEnc)
+        writer.WriteLine(output)
+        writer.Close()
+
+    End Sub
+
+
+End Class
+
+Public Class SampleData
+    Public Property Description As String
+    Public Property UpdateDate As DateTimeOffset
+    Public Property Data As Dictionary(Of Integer, String)
+End Class
+
+
